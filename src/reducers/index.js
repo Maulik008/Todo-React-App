9ca3c3db2c5c4c4a55c21e92400d4d6b8e03e0b5
Maulik008 <86708872+Maulik008@users.noreply.github.com>
2021-07-03 13:07:10 +0530

    Delete src directory

--- src/reducers/index.js
+++
@@ -1,9 +0,0 @@
-import { combineReducers } from 'redux'
-import todos from './todo'
-import Auth from './auth';
-
-const rootReducer = combineReducers({
-    todos, Auth
-})
-
-export default rootReducer;
